6ab33040c78b49c37728c2f3c6da0dd12ee6b83c
DH.L <meozigoon@gmail.com>
2025-04-27 22:00:07 +0900

    초기 커밋.

---
+++ Word Similarity/WordSimilarity.cs
@@ -0,0 +1,10 @@
+namespace Word_Similarity
+{
+    public partial class WordSimilarity : Form
+    {
+        public WordSimilarity()
+        {
+            InitializeComponent();
+        }
+    }
+}
